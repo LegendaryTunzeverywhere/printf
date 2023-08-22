54f7e1bfb6e7aecad4c1dcc20271cbb39b16ee29
OngeziweMzizi <ongeziwemzizi42@gmail.com>
2023-08-22 13:46:01 -0700

    Binary handler

--- unsigned_int.c
+++ unsigned_int.c
@@ -70,7 +70,7 @@ int print_o(va_list o)
  * @b: unsigned in to print
  *
  * Return: number of digits printed
- */
+ *
 int print_b(va_list b)
 {
 	unsigned int n, m, i, sum;
@@ -95,4 +95,37 @@ int print_b(va_list b)
 		}
 	}
 	return (count);
+}
+*/
+
+/**
+ * print_b - converts an unsigned int to binary and prints it
+ * @b: unsigned int to print in binary
+ *
+ * Return: number of digits printed
+ */
+int print_b(va_list b)
+{
+    unsigned int n = va_arg(b, unsigned int);
+    unsigned int a[32];
+    int i, sum, count;
+
+    // Convert to binary
+    for (i = 0; i < 32; i++)
+    {
+        a[i] = n % 2;
+        n /= 2;
+    }
+
+    // Print the binary representation
+    for (i = 31, sum = 0, count = 0; i >= 0; i--)
+    {
+        sum += a[i];
+        if (sum || i == 0)
+        {
+            _putchar('0' + a[i]);
+            count++;
+        }
+    }
+    return (count);
 }
